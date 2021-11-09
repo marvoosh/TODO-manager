e426ea5d353ff64a84803a737d61aaa707eac8d0
mn <marvoosh@gmail.com>
2021-11-09 13:58:24 +0000

    Finished no optional css

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # read all tasks (index)
   get '/tasks', to: 'tasks#index'
   # create a task (new + create)
-  get '/tasks/new', to: 'tasks#new', as: new_task
+  get '/tasks/new', to: 'tasks#new', as: :new_task
   post '/tasks', to: 'tasks#create'
   # read one task (show)
-  get 'tasks/:id', to: 'tasks#show', as: task
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # update a task (edit + update)
-  get '/tasks/:id/edit', to: 'tasks#edit', as: edit_task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch '/tasks/:id', to: 'tasks#update'
   # delete a task (destroy)
   delete 'tasks/:id', to: 'tasks#destroy'
